36cce804f9a371be8a43b77c59809ef8400f19f6
Chris3037 <lessdismissive@email.com>
2014-05-13 17:10:52 -0600

    V 2.0

---
+++ newBlog/newBlog/Scripts/main.js
@@ -0,0 +1,8 @@
+﻿//$(document).ready(function(){
+//    $('#nav2').on('mouseenter', function () {
+//        $('.nav2Items').fadeIn();
+//    });
+//    $('#nav2').on('mouseleave', function () {
+//        $('.nav2Items').fadeOut();
+//    });
+//});
